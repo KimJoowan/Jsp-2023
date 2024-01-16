0b39ca985afedab9787272948b5f7959a706a09a
KimJoowan <115052001+KimJoowan@users.noreply.github.com>
2024-01-16 15:48:28 +0900

    2 commit

--- main/java/cs/dit/mapper/MemberMapper.java
+++ main/java/cs/dit/mapper/MemberMapper.java
@@ -5,7 +5,7 @@ import cs.dit.domain.MemberDTO;
 public interface MemberMapper {
 	public int insert(MemberDTO member);
 	
-	public MemberDTO login(MemberDTO member);
+	public Boolean login(MemberDTO member);
 	
 	public MemberDTO selectone(String id);
 	
