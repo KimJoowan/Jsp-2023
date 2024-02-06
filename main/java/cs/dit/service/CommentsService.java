8a80fc9d78a81d6ae1c0b0e4b7f9135026f2e911
KimJoowan <115052001+KimJoowan@users.noreply.github.com>
2024-02-06 15:59:17 +0900

    3 commit

--- main/java/cs/dit/service/CommentsService.java
+++ main/java/cs/dit/service/CommentsService.java
@@ -5,7 +5,7 @@ import java.util.List;
 import cs.dit.domain.CommentsDTO;
 
 public interface CommentsService {
-	public List<CommentsDTO> list(int bcode);
+	public List<CommentsDTO> list(int bcode, Criteria cri);
 	
 	public Boolean register(int bcode, String content);
 }
