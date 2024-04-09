ff12b1cecc9805e596391f6dc2f9a5fe1d5d0d1f
KimJoowan <115052001+KimJoowan@users.noreply.github.com>
2024-04-09 13:25:29 +0900

    4 commit

--- main/java/cs/dit/controller/MemberController.java
+++ main/java/cs/dit/controller/MemberController.java
@@ -6,11 +6,13 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.ui.Model;
 
 import cs.dit.domain.MemberDTO;
@@ -27,15 +29,16 @@ public class MemberController {
 	@Autowired
 	private final MemberService service;
 	
+	@Autowired
+	BCryptPasswordEncoder passwordEncoder;
+	
 	@RequestMapping("joinmembership")
 	public String joinmembership() {
-		log.info("joinmembership");
 		return "/member/Memberadd";
 	}
 	
 	@RequestMapping("login")
 	public String login() {
-		log.info("login");
 		return "member/Login";
 	}
 	
@@ -46,113 +49,93 @@ public class MemberController {
 	
 	@RequestMapping("/logout")
 	public String logout(HttpServletRequest request) {
-		HttpSession session = request.getSession();
-		session.removeAttribute("sessionid");
-		return "redirect:/main/index";
+	    HttpSession session = request.getSession(false);
+	    
+	    if (session != null) {
+	        session.invalidate();
+	    }
+	    
+	    return "redirect:/main/index";
 	}
 	
 	@PostMapping("/idcheck")
 	@ResponseBody
 	public ResponseEntity<Boolean> confirmId(String id){
-		boolean result = true;
-		if(id.trim().isEmpty()) {
-			log.info("id : " + id);
-			result = false;
-		}else {
-			if (service.chack(id)) {
-				result = false;
-			} else {
-				result = true;
-			}
-		}
-		
-		return new ResponseEntity<>(result, HttpStatus.OK);
-	}
-	
-		
-	@PostMapping("/regiseter")
+		boolean result = service.idCheck(id);
+		return new ResponseEntity<>(result, HttpStatus.OK);	
+	}		
+				
+	@PostMapping("/register")
 	public String regiseter(MemberDTO member) {
-		log.info("regiseter");
-		int count = service.register(member);
-		
-		if(count == 1) {
+	
+		String encdoePassword = passwordEncoder.encode(member.getPwd());
+		member.setPwd(encdoePassword);
+				
+		if(service.register(member)) {
 			return"redirect:/main/index";
-		}else {			
-			return null;
 		}
+		
+		//보류
+		return null;
 	}
 	
 	@PostMapping("/check")
 	@ResponseBody
-	public ResponseEntity<Boolean> login(HttpServletRequest request, MemberDTO member) {		
-		log.info("check");
+	public ResponseEntity<Boolean> login(HttpServletRequest request, MemberDTO member) {
+		boolean result = false;	
+		MemberDTO getMember = service.get(member.getId());
 		
-		boolean result = service.check(member);
-		
+		if(getMember != null) {
+			result = passwordEncoder.matches(member.getPwd(), getMember.getPwd());
+		}
+								
 		if(result) {
-			HttpSession session = request.getSession();
+			// 로그인 성공 => 세션 생성
+			// 세션을 생성하기 전에 기존의 세션 파기
+			request.getSession().invalidate();
+			
+			 // Session이 없으면 생성
+			HttpSession session = request.getSession(true);
+			
+			 // 세션에 sessionid를 넣어줌
 			session.setAttribute("sessionid", member.getId());
-			result = true;
-		}else {
-			result = false;
-		}	
+			
+			// Session의 유효 시간 설정 (1800초 = 30분)
+			session.setMaxInactiveInterval(1800);	
+		}
+		
 		return new ResponseEntity<>(result, HttpStatus.OK);
 	}
 	
 	@GetMapping("/get")
-	public String get(HttpServletRequest request, Model model) {
-		log.info("get");
-		
-		HttpSession session = request.getSession();
-		String id = (String) session.getAttribute("sessionid");	
-		MemberDTO  Dto = new MemberDTO();
-		Dto = service.get(id);
-		log.info(Dto);
-		
-		if(Dto != null) {
-			model.addAttribute("Dto", Dto);
+	public String get(@SessionAttribute(name = "sessionid", required = false) String id, Model model) {		
+		if(id != null) {
+			model.addAttribute("Dto", service.get(id));
 			return "/member/Get";
-		}else {
-			return null;
-		}	
+		}
+		
+		return "redirect:/main/index";			
 	}
 	
 	@PostMapping("/modify")
 	public String modify(MemberDTO member) {
-		log.info("get");
+		if(service.modify(member)) {
+			return "redirect:/main/main";			
+		}
 		
-		int count = service.modify(member);
-		
-		if(count == 1) {
-			return "main/main";
-		}else {
-			return null;
-		}		
+		return "redirect:/main/index";	
 	}
 	
 	@RequestMapping("/remove")
-	public String remove(HttpServletRequest request) {
-		log.info("remove");
-		
-		HttpSession session = request.getSession();
-		String id = (String) session.getAttribute("sessionid");	
-		int count = service.remove(id);
-		
-		if(count == 1) {
-			return "main/index";
-		}else {
-			return null;
+	public String remove(@SessionAttribute(name = "sessionid", required = false) String id, HttpServletRequest request) {
+		 HttpSession session = request.getSession(false);
+		 
+		if(service.remove(id)) {
+			session.invalidate();
+			return "redirect:/main/index";
 		}
 		
+		return "redirect:/main/index";
 		
-	}
-		
-	
-	
-	
-	
-	
-	
-	
-	
+	}	
 }
