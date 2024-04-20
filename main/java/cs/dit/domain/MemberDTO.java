ece754e92d97d4f6f48bb827a94e67a8e57eda8d
KimJoowan <115052001+KimJoowan@users.noreply.github.com>
2024-04-20 10:53:00 +0900

    2 commit

--- main/java/cs/dit/domain/MemberDTO.java
+++ main/java/cs/dit/domain/MemberDTO.java
@@ -4,7 +4,7 @@ import lombok.Data;
 
 @Data
 public class MemberDTO {
-	private String id,pwd,email,name,address,phonenumber;
+	private String id,pwd,newpwd,email,name,address,phonenumber;
 	private int number;
 	
 }
