90856a75b956c3eca9f30e6ec1891023640602a1
KimJoowan <115052001+KimJoowan@users.noreply.github.com>
2024-04-20 10:53:54 +0900

    3 commit

--- main/java/cs/dit/mapper/MemberMapper.java
+++ main/java/cs/dit/mapper/MemberMapper.java
@@ -3,16 +3,18 @@ package cs.dit.mapper;
 import cs.dit.domain.MemberDTO;
 
 public interface MemberMapper {
-	public int insert(MemberDTO member);
+	public Boolean insert(MemberDTO member);
 	
 	public Boolean login(MemberDTO member);
 	
 	public MemberDTO selectone(String id);
 	
-	public int update(MemberDTO member);
+	public Boolean update(MemberDTO member);
 	
-	public int delete(String id);
-
-	public Boolean chack(String id);
+	public Boolean pwdupdate(MemberDTO member);
+	
+	public Boolean delete(String id);
+	
+	public Boolean idCheck(String id);
 
 }
