0c9850900f2848d98f1f1b189f5e059218ed2d8e
KimJoowan <115052001+KimJoowan@users.noreply.github.com>
2024-04-20 10:54:47 +0900

    3 commit

--- main/java/cs/dit/service/MemberService.java
+++ main/java/cs/dit/service/MemberService.java
@@ -3,16 +3,18 @@ package cs.dit.service;
 import cs.dit.domain.MemberDTO;
 
 public interface MemberService {
-	public int register(MemberDTO member);
+	public Boolean register(MemberDTO member);
 	
 	public Boolean check(MemberDTO member);
 	
 	public MemberDTO get(String id);
 	
-	public int modify(MemberDTO member);
+	public Boolean modify(MemberDTO member);
 	
-	public int remove(String id);
-
-	public Boolean chack(String id);
-
+	public Boolean pwdModify(MemberDTO member);
+	
+	public Boolean remove(String id);
+	
+	public Boolean idCheck(String id);
+	
 }
